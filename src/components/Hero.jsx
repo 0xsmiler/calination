1acd683bcca0978282e097b2a690ef3ad132cba5
0xsmiler <anthonylee110.eng@gmail.com>
2025-04-20 17:46:31 -0400

    change the uniswap link for the her section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -95,7 +95,7 @@ function Hero() {
           
           {/* Buy CNT Button */}
           <button
-            onClick={() => window.open("https://app.uniswap.org/explore/pools/base/0x086c01cd7891e8aed5fd27c01dcca6081b30318203aec3474c47464c030d9492", "_blank", "noopener,noreferrer")}
+            onClick={() => window.open("https://app.uniswap.org/explore/pools/base/0x5e73f39f4c3207c3d04498b1a25f5a896f63dfaa6b854ac14fe7433050963fe5", "_blank", "noopener,noreferrer")}
             className="bg-[#C68F00] hover:bg-[#b07e00] text-white font-semibold text-lg px-[80px] py-[20px] rounded-md transition"
           >
             Buy CNT

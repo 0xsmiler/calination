1c2a9c4254452c5241b7ee115c45390d729f2270
0xsmiler <anthonylee110.eng@gmail.com>
2025-04-22 01:41:28 -0400

    wagmiconfig net change to base

--- src/wagmiConfig.js
+++ src/wagmiConfig.js
@@ -1,15 +1,15 @@
 import { createConfig, http } from 'wagmi';
-import { mainnet, sepolia } from 'wagmi/chains';
+import { mainnet, base } from 'wagmi/chains';
 import { walletConnect, metaMask, coinbaseWallet } from '@wagmi/connectors';
 
 // Get projectId from environment variable with fallback for development
 const projectId = import.meta.env.VITE_WALLET_CONNECT_PROJECT_ID;
 
 export const config = createConfig({
-  chains: [mainnet, sepolia], // Add the chains you want to support
+  chains: [mainnet, base], // Add the chains you want to support
   transports: {
     [mainnet.id]: http(),
-    [sepolia.id]: http(),
+    [base.id]: http(),
   },
   connectors: [
     walletConnect({
